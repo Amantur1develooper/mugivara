04c6359fb139911244de116b9e1709b39be3b92a
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-01-31 11:24:38 +0600

    first commit12

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-(n20sb6gvpyx^q5#)!6b4v_mi11qkas(=j+l41nn_gzrtpx@6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
