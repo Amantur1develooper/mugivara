70508061efd6f4209a8a90ee6006625d2374484f
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-02-03 17:33:29 +0600

    234233

--- config/settings.py
+++ config/settings.py
@@ -197,6 +197,8 @@ REST_FRAMEWORK = {
     ],
 }
 
+TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN", "")
+
 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
