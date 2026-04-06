8d62ade2c0ea7cfd4714e4adadd4f6861ae342d9
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-04-06 19:39:44 +0600

    test3

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./public_site/templates/**/*.html",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
