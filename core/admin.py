3afb48fcc266a73d969cefc9605df2fe0e3ff0b6
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-04-13 22:14:03 +0600

    sdfsdfsdfsdfsdfwr23342323weвыа23235лощд

--- core/admin.py
+++ core/admin.py
@@ -177,9 +177,10 @@ class PromoCodeAdmin(admin.ModelAdmin):
 
 @admin.register(AdBanner)
 class AdBannerAdmin(admin.ModelAdmin):
-    list_display  = ("__str__", "button_text", "click_count", "is_active", "sort_order")
-    list_editable = ("is_active", "sort_order")
-    list_filter   = ("is_active", "button_style")
+    list_display       = ("__str__", "button_text", "click_count", "is_active", "sort_order")
+    list_display_links = ("__str__",)
+    list_editable      = ("is_active", "sort_order")
+    list_filter        = ("is_active", "button_style")
     readonly_fields = ("click_count",)
     fieldsets = (
         (None, {"fields": ("title", "is_active", "sort_order", "click_count")}),
